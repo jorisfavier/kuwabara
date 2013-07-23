ed2d253eb80693095b7bba956ace7d3b2efb85cb
jorisfavier <joris.favier@aliceadsl.fr>
2013-07-23 17:04:05 +0200

    mise en place de twig et gestion des mauvaise directions

--- kuwabara/src/kuwabara.php
+++ kuwabara/src/kuwabara.php
@@ -1,11 +1,45 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
- 
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 $app = new Silex\Application();
-$app->get('/', function() {
-    return 'Hello world!';
+
+//Mode debug activé
+$app['debug'] = true;
+
+
+/*** CONFIG DES ERREURS ******/
+$app->error(function(\Exception $e) use ($app) {
+    if ($e instanceof NotFoundHttpException) {
+        $content = vsprintf('<h1>%d - %s (%s)</h1>', array(
+           $e->getStatusCode(),
+           Response::$statusTexts[$e->getStatusCode()],
+           $app['request']->getRequestUri()
+        ));
+        return new Response($content, $e->getStatusCode());
+    }
+    if ($e instanceof HttpException) {
+        return new Response('<h1>You should go eat some cookies while we\'re fixing this feature!</h1>', $e->getStatusCode());
+    }
 });
- 
+
+/****** CONFIG DE TWIG ******/
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+
+
+/****** ROUTING ********/
+$app->get('/', function () use ($app) {
+   return $app['twig']->render('index.twig', array(
+        'hello' => "hello you",
+    ));
+});
+
 return $app;
 
 ?>
--- kuwabara/web/index.php
+++ kuwabara/web/index.php
@@ -1,11 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+$main = require_once __DIR__ . '/../src/kuwabara.php';
+$main->run();
 
-$app = new Silex\Application();
-
-$app->get('/hello', function() {
-    return 'Hello!';
-});
-
-$app->run();
+?>
